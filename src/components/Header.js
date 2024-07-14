200f1fa53c28d17516c4cc0730276b6fe017ad2b
Prabhanshu-Yadav <147831437+Prabhanshu-Yadav@users.noreply.github.com>
2024-07-14 12:16:33 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+
+  
+
+  return (
+    <div className="header">
+      <div className="header-logo">Algo<span>Visuals</span></div>
+   </div>
+  )
+}
+
+export default Header;
